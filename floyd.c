5750409dbda2339b950d322ab9288f558feca95d
nightwolf212 <nightwolf212@mail.ru>
2014-10-17 22:53:17 +0900

    Refactoring. Step 1
    Add new methods. To be continued.

--- floyd.c
+++ floyd.c
@@ -33,45 +33,74 @@ typedef struct cell
     long long value;
 } cell ;
 
+
 int min(long long a, long long b)
 {
     return a < b ? a : b;
 }
-
-int main()
+struct cell *rowCell;
+struct cell *head=NULL;
+struct cell *rightEnd;
+struct cell *downEnd;
+struct cell *prev;
+struct cell* getNewCell(void)
 {
-    int n;
-    //scanf("%i", &n);
-    struct cell *head=(cell *) malloc( sizeof(cell));
-    head->down=NULL;
-    head->left=NULL;
-    head->right=NULL;
-    head->up=NULL;
-    struct cell *curr=head;
-    struct cell *rowCell=head;
-    struct cell *prev=NULL;
-    struct cell *rightEnd=head;
-    struct cell *downEnd=head;
-    int wasFirst=0;
-    while(1)                // Добоавление вершин в матрицу смежности. Каждый раз, имеющаяся матрица смежности увеличивает количество столбцов и строк на указаное число.
-    {
-        int b;
-        printf("Укажите число вершин, которое желаете добавить ( -1  закончить ввод)\n");
-        scanf("%i", &b);
-        if(b==-1)
-            break;
-        curr=rightEnd;
-        prev=NULL;
-        rowCell=rightEnd;
-        int cnt=0;
-        if(wasFirst)                               // Случай, если уже было произведено добавление хотя бы одной вершины.
+	 struct cell* c=(cell *) malloc( sizeof(cell));
+	 c->down=NULL;
+	 c->left=NULL;
+	 c->right=NULL;
+	 c->up=NULL;
+	 return c;
+}
+void addNewVertex(int cnt)
+{
+	if(head==NULL)
+	{
+		struct cell *curr=head;
+		head=getNewCell();
+		rightEnd=head;
+		downEnd=head;
+	    int i;
+        for( i=0; i<cnt; i++)              //первое добавление вершин, формируется начальная квадратная матрица, которая в дальнейшем будет расширяться.
         {
-            struct cell *newHead=(cell *)malloc( sizeof(cell));
-            newHead->down=NULL;
-            newHead->left=NULL;
-            newHead->right=NULL;
-            newHead->up=NULL;
+            int j;
+            for(j=0; j<cnt; j++)
+            {
+                curr->down=getNewCell();
+                curr->down->up=curr;
+                if(i==0||j==cnt-1)
+                {
+                    curr->right=getNewCell();
+                    curr->right->left=curr;
+                    if(i!=0)
+                    {
+                        curr->right->up=curr->up->right;
+                        curr->up->right->down=curr->right;
+                    }
+                }
+                if(prev!=NULL)
+                {
+                    prev->down->right=curr->down;
+                    curr->down->left=prev->down;
+                    prev->down->right->up=curr;
+                }
+                    prev=curr;
+                curr=curr->right;
+                if(i==0)
+                    rightEnd=curr;
+            }
+			curr=rowCell->down;
+			rowCell=rowCell->down;
+			prev=NULL;
+			// if(!wasFirst)
+				 downEnd=rowCell;
+		 }
+      //  wasFirst=1;
 
+	 }
+	else
+	{
+			struct cell *newHead=getNewCell();
             struct cell *newCurr=newHead;
             struct cell *newRowCell=newHead;
             struct cell *newPrev=NULL;
@@ -82,23 +111,13 @@ int main()
             while(1)
             {
                 int j;
-            for(j=0; j<b; j++)                   // создание матрицы прямоуголька NxM , где М- количество вериш для добавления, N - текущее количество вершин
+            for(j=0; j<cnt; j++)                   // создание матрицы прямоуголька NxM , где М- количество вериш для добавления, N - текущее количество вершин
             {
-                newCurr->down=(cell *)malloc( sizeof(cell));
-                newCurr->down->down=NULL;
-                newCurr->down->left=NULL;
-                newCurr->down->right=NULL;
-                newCurr->down->up=NULL;
-
+                newCurr->down=getNewCell();
                 newCurr->down->up=newCurr;
-                if(!wf||j==b-1)
+                if(!wf||j==cnt-1)
                 {
-                    newCurr->right=(cell *)malloc( sizeof(cell));
-                    newCurr->right->down=NULL;
-                    newCurr->right->left=NULL;
-                    newCurr->right->right=NULL;
-                    newCurr->right->up=NULL;
-
+                    newCurr->right=getNewCell();
                     newCurr->right->left=newCurr;
                     if(wf)
                     {
@@ -112,7 +131,7 @@ int main()
                     newCurr->down->left=newPrev->down;
                     newPrev->down->right->up=newCurr;
                 }
-                    newPrev=newCurr;
+                newPrev=newCurr;
                 newCurr=newCurr->right;
                 if(!wf)
                     newRightEnd=newCurr;
@@ -138,35 +157,24 @@ int main()
         rightEnd=newRightEnd;
 
 
-             newHead=(cell *)malloc( sizeof(cell));                                 // генерация нижней половины матрицы. Данная половина будет иметь размер XxY где X - количество вершин для добавления
+             newHead=getNewCell();                                 // генерация нижней половины матрицы. Данная половина будет иметь размер XxY где X - количество вершин для добавления
              newCurr=newHead;                                   // Y = Х + Old, где Old - количество вершин в графе до добавления вершин (Y - новый размер матрицы)
              newRowCell=newHead;
              newPrev=NULL;
-             newHead->down=NULL;
-             newHead->left=NULL;
-             newHead->right=NULL;
-             newHead->up=NULL;
+            
              struct cell *newDownEnd=NULL;
              wf=0;
             struct cell *width=head;
 
-            for(i=0; i<b; i++)
+            for(i=0; i<cnt; i++)
             {
                 while(1)
                 {
-                    newCurr->down=(cell *)malloc( sizeof(cell));
-                    newCurr->down->down=NULL;
-                    newCurr->down->left=NULL;
-                    newCurr->down->right=NULL;
-                    newCurr->down->up=NULL;
+                    newCurr->down=getNewCell();
                     newCurr->down->up=newCurr;
                     if(i==0||width==rightEnd->left)
                     {
-                        newCurr->right=(cell *)malloc( sizeof(cell));
-                        newCurr->right->down=NULL;
-                        newCurr->right->left=NULL;
-                        newCurr->right->right=NULL;
-                        newCurr->right->up=NULL;
+                        newCurr->right=getNewCell();
                         newCurr->right->left=newCurr;
                         if(i!=0)
                         {
@@ -208,59 +216,21 @@ int main()
         }
 
             downEnd=newDownEnd;
+	}
+}
 
+int main(int argc, char * argv[])
+{
+	
 
-        }
-
-
-        else
-        {
-            int i;
-        for( i=0; i<b; i++)              //первое добавление вершин, формируется начальная квадратная матрица, которая в дальнейшем будет расширяться.
-        {
-            int j;
-            for(j=0; j<b; j++)
-            {
-                curr->down=(cell *)malloc( sizeof(cell));
-                curr->down->down=NULL;
-                curr->down->left=NULL;
-                curr->down->right=NULL;
-                curr->down->up=NULL;
-                curr->down->up=curr;
-                if(i==0||j==b-1)
-                {
-                    curr->right=(cell *)malloc( sizeof(cell));
-                    curr->right->down=NULL;
-                    curr->right->left=NULL;
-                    curr->right->right=NULL;
-                    curr->right->up=NULL;
-                    curr->right->left=curr;
-                    if(i!=0)
-                    {
-                        curr->right->up=curr->up->right;
-                        curr->up->right->down=curr->right;
-                    }
-                }
-                if(prev!=NULL)
-                {
-                    prev->down->right=curr->down;
-                    curr->down->left=prev->down;
-                    prev->down->right->up=curr;
-                }
-                    prev=curr;
-                curr=curr->right;
-                if(i==0)
-                    rightEnd=curr;
-            }
-        curr=rowCell->down;
-        rowCell=rowCell->down;
-        prev=NULL;
-        if(!wasFirst)
-            downEnd=rowCell;
-        }
-        wasFirst=1;
-
-    }
+    while(1)                // Добоавление вершин в матрицу смежности. Каждый раз, имеющаяся матрица смежности увеличивает количество столбцов и строк на указаное число.
+    {
+        int b;
+        printf("Укажите число вершин, которое желаете добавить ( -1  закончить ввод)\n");
+        scanf("%i", &b);
+        if(b==-1)
+            break;
+       addNewVertex(b);
     }
     curr=head;
     rowCell=head;
